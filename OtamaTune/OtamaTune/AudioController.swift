b97d19c4e747b4665a4aaea4a623286754cd05e9
Kouki Saito <dan.addr.skd@gmail.com>
2014-08-31 00:27:28 +0900

    Add main files

---
+++ OtamaTune/OtamaTune/AudioController.swift
@@ -0,0 +1,17 @@
+//
+//  AudioController.swift
+//  OtamaTune
+//
+//  Created by Kouki Saito on 2014/08/31.
+//  Copyright (c) 2014年 Kouki. All rights reserved.
+//
+
+import Foundation
+
+
+class AudioController{
+    
+    init(){
+        
+    }
+}
